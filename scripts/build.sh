9b785ce68011e817e2aa3fe4d802da2373b9d843
anuranjnp2-ui <anuranjnp2@gmail.com>
2026-03-30 17:36:58 +0530

    your message here

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,6 @@
 # exit on error
 set -o errexit
 
-echo "--- Building Frontend ---"
-cd frontend
-npm install
-npm run build
-cd ..
-
-echo "--- Installing Backend Dependencies ---"
-# Use pip/uv to install requirements
 # Use pip/uv to install requirements
 pip install -r backend/requirements.txt
 
